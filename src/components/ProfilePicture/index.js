695a30b3c30bb21b65e6c36840cb30f735b96f63
sarvanideekshitula <sarvanimini@gmail.com>
2020-02-10 08:26:59 +0530

    Added Component for ProfilePicture

---
+++ src/components/ProfilePicture/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function Image() {
+  return (
+    <div className="ProfilePicture">
+      <img src="https://www.w3schools.com/w3images/avatar6.png" alt="Profilepic" />
+    </div>
+  );
+}
+
+export default Image;
